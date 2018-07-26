3b699dc0c6a5c8a2b9510478b6595deaafdf2749
Zac McClung <zac.mcclung370@gmail.com>
2018-07-26 17:03:20 -0400

    intial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+// import Background from './media/jeremy-bishop-14583-unsplash.jpg';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div>
+          <h1>Coming Soon!</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Test test test</h3>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
